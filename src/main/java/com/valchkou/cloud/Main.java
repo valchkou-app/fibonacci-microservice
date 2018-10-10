d027919d0099f352dac3a462cbf3f1960244ff0f
evalchkou <evgeny.valchkou@imanage.com>
2018-10-10 09:08:34 -0500

    integration with admin server

--- src/main/java/com/valchkou/cloud/Main.java
+++ src/main/java/com/valchkou/cloud/Main.java
@@ -3,10 +3,14 @@ package com.valchkou.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
 public class Main {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
 	}
 }
+
+
--- src/main/java/com/valchkou/cloud/web/FibonacciController.java
+++ src/main/java/com/valchkou/cloud/web/FibonacciController.java
@@ -6,6 +6,8 @@ import com.valchkou.cloud.service.FibonacciService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +15,8 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/fibonacci")
 public class FibonacciController {
+
+    Logger log = LoggerFactory.getLogger(this.getClass().getName());
 
     @Autowired
     FibonacciService fibonacciService;
@@ -28,6 +32,7 @@ public class FibonacciController {
     @ResponseBody FibonacciResult calcN(@PathVariable int nth,
                             @ApiParam(value = "Calc Mode", required = true)
                             @PathVariable(required = true) CalcMode mode) {
+        log.info("calculate number {} using alg {}", nth, mode);
         return fibonacciService.calcNthNumber(nth, mode);
     }
 }
